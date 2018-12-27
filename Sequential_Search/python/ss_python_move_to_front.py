4e0f1c999657c47a9cb30d05d0093ef1e587e470
Guilherme AugustoD <guilherme.francais@gmail.com>
2018-12-27 15:51:45 -0200

    Creates sequential search by move to front algorithm

---
+++ Sequential_Search/python/ss_python_move_to_front.py
@@ -0,0 +1,25 @@
+import random
+
+elements = random.sample(range(1000), 10)
+
+print('There is a list of elements builded!')
+
+print(elements)
+
+number = int(input('Say a number to find in the array container: '))
+
+found = False
+
+for i in range(0, len(elements)):
+    if elements[i] == number:
+        tmp = elements[0]
+        elements[0] = elements[i]
+        elements[i] = tmp
+        found = True
+
+if not found:
+    print('The element {} was not found!'.format(number))
+else:
+    print('The element {} was found!'.format(number))
+    print('There is the new list ordered by move to front algorithm!')
+    print(elements)
